a5e519d802a21c50dbfa65f7916c1cd6fc438efc
Dominic Meddick <dominicmeddick@gmail.com>
2021-05-05 16:48:49 +0100

    Finished Fizz with Finn

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,14 +1,29 @@
 describe('Fizzbuzz',function() {
 
   var fizzbuzz; 
+  beforeEach(function() {
+    fizzbuzz = new FizzBuzz();
+  })
 
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
-
+    it('NOT divisible by 3', function() {
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+    it('divisible by 5', function(){
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+    it('NOT divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
+    });
+    it('divisible by 15', function() {
+      expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+    it('NOT divisible by 15', function() {
+      expect(fizzbuzz.isDivisibleByThreeAndFive(5)).toBe(false);
+    });
   });
-
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,6 +1,30 @@
 class FizzBuzz {
+  isDivisibleByThreeAndFive(number) {
+    return number % 3 === 0 && number % 5 ===0;
+  }
   isDivisibleByThree(number) {
-    return true;
+    return number % 3 === 0 
+  }
+  isDivisibleByFive(number) {
+    return number % 5 === 0
+  }
+
+  says(number) {
+    if (this.isDivisibleByThreeAndFive(number)) {
+      return 'Fizzbuzz'
+    } 
+    else if (this.isDivisibleByThree(number)) {
+      return 'Fizz'
+   }
+   else if (this.isDivisibleByFive(number)) {
+      return 'Buzz'
+     }
+   else {
+      return number
+   }
   }
 }
-
+fizzbuzz = new FizzBuzz;
+for (let i = 0; i <= 100; i++) {
+ console.log(fizzbuzz.says(i));
+}
